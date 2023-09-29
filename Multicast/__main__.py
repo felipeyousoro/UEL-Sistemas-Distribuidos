145ec9ce1e94998862427acc853d438d9f7dfc95
felipe "based mito" yousoro <42080804+felipeyousoro@users.noreply.github.com>
2023-09-28 21:21:13 -0300

    agr vai

--- Multicast/__main__.py
+++ Multicast/__main__.py
@@ -18,7 +18,7 @@ def get_peers(file: str) -> list:
 
     return peers
 
-def usr1():
+if __name__ == '__main__':
     client = clnt.Client(IP, 'client1')
 
     peers = get_peers('test1.txt')
@@ -28,18 +28,4 @@ def usr1():
     for peer in peers:
         client.add_peer(peer)
 
-def usr2():
-    client = clnt.Client(socket.gethostbyname_ex(HOSTNAME)[2][1], 'client2')
-
-    peers = get_peers('tmptrash/test2.txt')
-
-    threading.Thread(target=client.run).start()
-
-    for peer in peers:
-        client.add_peer(peer)
-
-if __name__ == '__main__':
-    usr1()
-    # time.sleep(1)
-    # usr2()
 
